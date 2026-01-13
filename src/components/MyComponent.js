2340d6a7b9a199f883a74a005107cf674d5c043f
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-13 18:10:21 +0700

    19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,12 +5,16 @@ class MyComponent extends React.Component {
       address : 'Hà Nội',
       age : 22
     };  
+    handleClick = () => {
+      console.log('Button clicked!');
+    }
   //jsx
   render() {
 
     return (
       <div>
         My name is {this.state.name} and im from {this.state.address}, im {this.state.age} years old.
+        <button onClick={this.handleClick}>Click me</button>
       
       </div>
     );
