8b6390dd489343beec0ef490461873da91b23dd8
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-13 11:09:49 +0700

    2

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,17 @@
 import React from 'react';
 class MyComponent extends React.Component {
+  state = {
+      name : 'Hoàng',
+      address : 'Hà Nội',
+      age : 22
+    };  
+  //jsx
   render() {
+
     return (
       <div>
-        Hello from MyComponent
-        {Math.random()}
+        My name is {this.state.name} and im from {this.state.address}, im {this.state.age} years old.
+      
       </div>
     );
   }
