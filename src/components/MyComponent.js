5fbf0c6ff41a2dab2ad405b439b8ef76c992c53e
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-14 11:21:59 +0700

    22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,49 +1,10 @@
-import React from 'react';
-import Userinfor from './Userinfor';
+import React from "react";
+import Userinfor from "./Userinfor";
 class MyComponent extends React.Component {
-  state = {
-      name : 'Hoàng',
-      address : 'Hà Nội',
-      age : 22
-    };  
-   
-    handleOnChangeInput = (event) => {
-      this.setState({
-        name : event.target.value
-      });
-    }
-    handleOnChangeAge = (event) => {
-      this.setState({
-        age : event.target.value
-      });
-    }
-
-    handleOnSubmit = (event) => {
-      event.preventDefault();
-      console.log('>>> check submit:', this.state.name);
-    }
   //jsx
   render() {
-
     return (
       <div>
-
-        My name is {this.state.name} and im from {this.state.address}, im {this.state.age} years old.
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <label>Name:</label>
-          <input 
-          value={this.state.name}
-          type="text" 
-          onChange={(event) => this.handleOnChangeInput(event)}
-          />
-          <label>Age:</label>
-          <input 
-          value={this.state.age}
-          type="text" 
-          onChange={(event) => this.handleOnChangeAge(event)}
-          />
-          <button>Submit</button>
-        </form>
         <Userinfor />
       </div>
     );
--- src/components/Userinfor.js
+++ src/components/Userinfor.js
@@ -1,10 +1,49 @@
 import React from "react";
 
 class Userinfor extends React.Component {
+  state = {
+    name: "Hoàng",
+    address: "Hà Nội",
+    age: 22,
+  };
+
+  handleOnChangeInput = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleOnChangeAge = (event) => {
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(">>> check submit:", this.state.name);
+  };
   render() {
     return (
-      <div>im a child </div>
-    )
+      <div>
+        My name is {this.state.name} and im from {this.state.address}, im{" "}
+        {this.state.age} years old.
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <label>Name:</label>
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeInput(event)}
+          />
+          <label>Age:</label>
+          <input
+            value={this.state.age}
+            type="text"
+            onChange={(event) => this.handleOnChangeAge(event)}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
