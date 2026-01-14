300d97f79838818fef9238ede4533e4f5810e025
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-15 05:03:05 +0700

    25

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,48 +1,23 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  handleShowHide = () => {
+    alert("click me");
+  };
   render() {
     const { listUsers } = this.props;
-    console.log(listUsers);
     return (
       <div>
-        {listUsers.map((user, index) => {
-          console.log(">>> check user:", user);
-
-          return (
-            <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-              <div>My name is {user.name}</div>
-              <div>Im {user.age} years old</div>
-              <hr />
-            </div>
-          );
-
-          // if (+user.age > 18) {
-          //   return (
-          //     <div key={user.id} className="green">
-          //       <div>My name is {user.name}</div>
-          //       <div>Im {user.age} years old</div>
-          //       <hr />
-          //     </div>
-          //   );
-          // } else {
-          //   return (
-          //     <div key={user.id} className="red">
-          //       <div>My name is {user.name}</div>
-          //       <div>Im {user.age} years old</div>
-          //       <hr />
-          //     </div>
-          //   );
-          // }
-        })}
-        {/* <div>My name is {name}</div>
-        <div>Im {age} years old</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>Im {age} years old</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>Im {age} years old</div> */}
+        <div>
+          <span onClick={() => this.handleShowHide()}>Hide List Users: </span>
+        </div>
+        {listUsers.map((user) => (
+          <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+            <div>My name is {user.name}</div>
+            <div>Im {user.age} years old</div>
+            <hr />
+          </div>
+        ))}
       </div>
     );
   }
