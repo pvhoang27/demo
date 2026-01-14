b57ec561414fd6a2f83e0ea78acb9c19d80df74f
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-14 12:56:05 +0700

    23

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class DisplayInfor extends React.Component {
+  render() {
+    return (
+      <div> display infor </div>
+    )
+  }
+}
+
+export default DisplayInfor;
