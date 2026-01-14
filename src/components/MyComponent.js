9dd77d48a5d6dbdf2dd258304e6ead8a7d97acab
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-14 11:02:07 +0700

    21

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,38 +1,42 @@
-import React from "react";
+import React from 'react';
 class MyComponent extends React.Component {
   state = {
-    name: "Hoàng",
-    address: "Hà Nội",
-    age: 22,
-  };
-  handleClick = (event) => {
-    console.log("Button clicked!");
-    // console.log("My name is", this.state.name);
-    // console.log("random",);
-
-    this.setState({
-      name: "Nguyễn Văn A",
-      age: Math.floor(Math.random() * 100),
-    });
-    // this.setState({
-    // age : Math.floor(Math.random() * 100)
-    // });
-  };
-
-  handleOnMouseOver(event) {
-    // console.log(event.pageX);
-  }
-  handleOnChangeInput = (event) => {
-    console.log(event.target.value);
-  };
+      name : 'Hoàng',
+      address : 'Hà Nội',
+      age : 22
+    };  
+    handleClick= (event ) => {
+      console.log('Button clicked!');
+      // console.log("My name is", this.state.name);
+      // console.log("random",);
+      
+      this.setState({
+        name : 'Nguyễn Văn A',
+        age : Math.floor(Math.random() * 100)
+      });
+      // this.setState({
+        // age : Math.floor(Math.random() * 100)
+      // });
+    }
+    
+    handleOnMouseOver (event) {
+      // console.log(event.pageX);
+    }
+    handleOnChangeInput = (event) => {
+      console.log(event, event.target.value);
+    }
   //jsx
   render() {
+
     return (
       <div>
-        My name is {this.state.name} and im from {this.state.address}, im{" "}
-        {this.state.age} years old.
+
+        My name is {this.state.name} and im from {this.state.address}, im {this.state.age} years old.
         <form>
-          <input type="text" onChange={this.handleOnChangeInput} />
+          <input 
+          type="text" 
+          onChange={(event) => this.handleOnChangeInput(event)}
+          />
           <button>Submit</button>
         </form>
       </div>
