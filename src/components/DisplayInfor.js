5c47efff0d0068c5dcc45623f6ae19d59bb402c0
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-16 13:45:55 +0700

    34

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,15 +1,39 @@
 import React from "react";
 import "./DisplayInfor.scss";
 import logo1 from "../logo.svg";
-class DisplayInfor extends React.Component {
+// class DisplayInfor extends React.Component {
   
+//   render() {
+//     console.log(">>> call me render ");
+//     const { listUsers } = this.props;
+//     return (
+//       <div className="display-infor-container">
+      
+//         {true && (
+//           <>
+//             {listUsers.map((user, index) => (
+//               <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                 <div>My name is {user.name}</div>
+//                 <div>Im {user.age} years old</div>
+//                 <div>
+//                   <button onClick={() => this.props.handleDeleteUser(user.id)}>
+//                     Delete
+//                   </button>
+//                 </div>
+//                 <hr />
+//               </div>
+//             ))}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  
-
- 
-  render() {
-    console.log(">>> call me render ");
+const DisplayInfor = () => {
     const { listUsers } = this.props;
+
+    
     return (
       <div className="display-infor-container">
       
@@ -31,7 +55,6 @@ class DisplayInfor extends React.Component {
         )}
       </div>
     );
-  }
+  
 }
-
 export default DisplayInfor;
