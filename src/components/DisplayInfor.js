691730a31b0c8f966eb3dda4f7471ff2b0620fd9
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-16 13:49:25 +0700

    34

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./DisplayInfor.scss";
 import logo1 from "../logo.svg";
 // class DisplayInfor extends React.Component {
-  
+
 //   render() {
 //     console.log(">>> call me render ");
 //     const { listUsers } = this.props;
 //     return (
 //       <div className="display-infor-container">
-      
+
 //         {true && (
 //           <>
 //             {listUsers.map((user, index) => (
@@ -31,29 +31,27 @@ import logo1 from "../logo.svg";
 // }
 
 const DisplayInfor = (props) => {
-    const { listUsers } = props;
+  const { listUsers } = props;
 
-    return (
-      <div className="display-infor-container">
-      
-        {true && (
-          <>
-            {listUsers.map((user, index) => (
-              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                <div>My name is {user.name}</div>
-                <div>Im {user.age} years old</div>
-                <div>
-                  <button onClick={() => props.handleDeleteUser(user.id)}>
-                    Delete
-                  </button>
-                </div>
-                <hr />
+  return (
+    <div className="display-infor-container">
+      {true && (
+        <>
+          {listUsers.map((user, index) => (
+            <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+              <div>My name is {user.name}</div>
+              <div>Im {user.age} years old</div>
+              <div>
+                <button onClick={() => props.handleDeleteUser(user.id)}>
+                  Delete
+                </button>
               </div>
-            ))}
-          </>
-        )}
-      </div>
-    );
-  
-}
+              <hr />
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
 export default DisplayInfor;
