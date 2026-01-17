cdacf148fbd74fa2f90f2619fb7d8e36c5920f02
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-17 14:07:09 +0700

    50

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,19 +10,28 @@ const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Hoang</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">Hoang</Navbar.Brand> */}
+        <NavLink to="/" className="navbar-brand">
+          Hoang
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/" className="nav-link">Home</NavLink>
-            <NavLink to="/users" className="nav-link">User</NavLink>
-            <NavLink to="/admins" className="nav-link">Admin</NavLink>
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/users" className="nav-link">
+              User
+            </NavLink>
+            <NavLink to="/admins" className="nav-link">
+              Admin
+            </NavLink>
           </Nav>
           <Nav>
             <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              <NavDropdown.Item>Log in</NavDropdown.Item>
+              <NavDropdown.Item>Log out</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
