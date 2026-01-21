873d3c0c8b39483eefd0cd7348dcab18aa065479
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-21 09:44:45 +0700

    63

--- src/components/Admin/Content/ManagerUser.js
+++ src/components/Admin/Content/ManagerUser.js
@@ -7,68 +7,12 @@ import { FcPlus } from "react-icons/fc";
 import TableUser from "./TableUser";
 import { useEffect } from "react";
 import { getAllUsers } from "../../../service/apiService";
+import ModalUpdateUser from "./ModalUpdateUser";
 
-// function Example() {
-//   const [show, setShow] = useState(false);
-
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-
-//   return (
-//     <>
-//       <Button variant="primary" onClick={handleShow}>
-//         Launch demo modal
-//       </Button>
-
-//       <Modal show={show} onHide={handleClose} size="xl">
-//         <Modal.Header closeButton>
-//           <Modal.Title>Add new user</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <form className="row g-3">
-//             <div className="col-md-6">
-//               <label className="form-label">Email</label>
-//               <input type="email" className="form-control" />
-//             </div>
-//             <div className="col-md-6">
-//               <label className="form-label">Password</label>
-//               <input type="password" className="form-control" />
-//             </div>
-
-//             <div className="col-md-6">
-//               <label className="form-label">Username</label>
-//               <input type="text" className="form-control" />
-//             </div>
-//             <div className="col-md-4">
-//               <label className="form-label">Role</label>
-//               <select className="form-select">
-//                 <option selected value="USER">
-//                   USERS
-//                 </option>
-//                 <option value="ADMIN"> ADMIN</option>
-//               </select>
-//             </div>
-//             <div className="col-md-12">
-//               <label className="form-label">Image</label>
-//               <input type="file" />
-//             </div>
-//           </form>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleClose}>
-//             Close
-//           </Button>
-//           <Button variant="primary" onClick={handleClose}>
-//             Save
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </>
-//   );
-// }
 
 const ManagerUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
+  const [showModalUpdateUser, setShowModalUpdateUser] = useState(false);
   const [listUsers, setListUsers] = useState([]);
   useEffect(async () => {
     fetchListUsers();
@@ -110,6 +54,9 @@ const ManagerUser = (props) => {
           show={showModalCreateUser}
           setShow={setShowModalCreateUser}
           fetchListUsers={fetchListUsers}
+        />
+        <ModalUpdateUser 
+        show={showModalUpdateUser}
         />
       </div>
     </div>
