114ccc1bfcfee7ba09b3cd894cb77d9eee93d8bc
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-21 11:08:10 +0700

    64

--- src/components/Admin/Content/ModalUpdateUser.js
+++ src/components/Admin/Content/ModalUpdateUser.js
@@ -50,6 +50,14 @@ const ModalUpdateUser = (props) => {
     }
   };
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  };
+
   const handleSubmitCreateUser = async () => {
     //validate
     const isValidEmail = validateEmail(email);
