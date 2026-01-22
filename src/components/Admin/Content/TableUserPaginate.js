0ed63a99c250f6f6ddc332cb174476cdd8acebc9
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-22 11:30:00 +0700

    69

--- src/components/Admin/Content/TableUserPaginate.js
+++ src/components/Admin/Content/TableUserPaginate.js
@@ -54,7 +54,7 @@ const TableUserPaginate = (props) => {
             })}
           {listUsers && listUsers.length === 0 && (
             <tr>
-              <td colSpan={"4"}>Not found data</td>
+              <td colSpan={"5"}>Not found data</td>
             </tr>
           )}
         </tbody>
