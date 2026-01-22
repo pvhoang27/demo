b206cf89abfcdbd47e96986254fb413eea0dd6c0
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-22 14:15:50 +0700

    70

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -30,5 +30,15 @@ const deleteUser = (userId) => {
 const getUserWithPaginate = (page, limit) => {
   return axios.get(`/api/v1/participant?page=${page}&limit=${limit}`);
 };
+const postLogin = (userEmail, userPassword) => {
+  return axios.post(`/api/v1/login`, 
+    { email: userEmail, password: userPassword }
+  );
+}
 
-export { postCreateNewUser, getAllUsers, PutUpdateUser, deleteUser, getUserWithPaginate };
+// const postLogin1 = (email, password) => {
+//   return axios.post(`/api/v1/login`, 
+//     { email,  password }
+//   );
+// }
+export { postCreateNewUser, getAllUsers, PutUpdateUser, deleteUser, getUserWithPaginate, postLogin };
