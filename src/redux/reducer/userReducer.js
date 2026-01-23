782d837f55d835ef2196a116fe9095dfb026c5f0
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-23 14:23:49 +0700

    78

--- src/redux/reducer/userReducer.js
+++ src/redux/reducer/userReducer.js
@@ -1,28 +1,37 @@
-
-import { INCREMENT, DECREMENT } from '../action/counterAction';
+import { INCREMENT, DECREMENT } from "../action/counterAction";
 const INITIAL_STATE = {
-    account:{
-        access_token:'',
-        refresh_token:'',
-        username:'',
-    },
-    isAuthenticated: false,
-    
+  account: {
+    access_token: "",
+    refresh_token: "",
+    username: "",
+    image: "",
+    roles: "",
+  },
+  isAuthenticated: false,
 };
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case 'FETCH_USER_LOGIN_SUCCESS':
-            console.log('check action ', action)
-            return {
-                ...state, count: state.count + 1,
-            };
+  switch (action.type) {
+    case "FETCH_USER_LOGIN_SUCCESS":
+      console.log("check action ", action);
+      return {
+        ...state,
+        account: {
+          access_token: action?.playload?.DT?.access_token,
+          refresh_token: action?.playload?.DT?.refresh_token,
+          username: action?.playload?.DT?.username,
+          image: action?.playload?.DT?.image,
+          roles: action?.playload?.DT?.roles,
+        },
+      };
 
-        case DECREMENT:
-            return {
-                ...state, count: state.count - 1,
-            };
-        default: return state;
-    }
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
