f99c2b15d70e347832f19c19f4b1e4b2b0f813b0
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-23 14:34:19 +0700

    78

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -4,8 +4,6 @@ const HomePage = (props) => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const account = useSelector((state) => state.user.account);
 
-  console.log("account : ", account, " isAuthenticated: ", isAuthenticated);
-
   return (
     <div className="homepage-container">
       <video autoPlay muted loop>
