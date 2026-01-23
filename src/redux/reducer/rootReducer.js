73e67c1d294ce12fe1e7904655fdf3a156231beb
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-23 14:14:42 +0700

    78

--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -3,6 +3,7 @@ import counterReducer from './counterReducer';
 import userReducer from './userReducer';
 const rootReducer = combineReducers({
     counter: counterReducer,
+    userEric : userReducer
 });
 
 export default rootReducer;
