6b6b86050c440585960c482ec2ae0fe10df77d6c
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-24 11:23:25 +0700

    79

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import NProgress from "nprogress";
+
 const instance = axios.create({
   baseURL: "http://localhost:8081/",
 });
@@ -18,27 +20,33 @@ instance.interceptors.request.use(
 );
 
 // Add a request interceptor
-instance.interceptors.request.use(function (config) {
+instance.interceptors.request.use(
+  function (config) {
+    NProgress.start();
     // Do something before request is sent
     return config;
-  }, function (error) {
+  },
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
-  });
+  },
+);
 
 // Add a response interceptor
-instance.interceptors.response.use(function (response) {
+instance.interceptors.response.use(
+  function (response) {
+    NProgress.done();
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response && response.data ? response.data : response;
-  }, function (error) {
+  },
+  function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     console.log(">>>interceptors error: ", error.response);
     return error && error.response && error.response.data
-    ?
-     error.response.data 
-     :
-      Promise.reject(error);
-  });
+      ? error.response.data
+      : Promise.reject(error);
+  },
+);
 export default instance;
