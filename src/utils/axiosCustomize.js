368f7a4f1e412030a51913cea39c2db0526253d8
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-30 18:24:07 +0700

    81

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import NProgress from "nprogress";
-
+import {store} from "../redux/store";
 NProgress.configure({
   showSpinner: false,
 
@@ -28,6 +28,8 @@ instance.interceptors.request.use(
 // Add a request interceptor
 instance.interceptors.request.use(
   function (config) {
+    const access_token = store?.getState()?.user?.account?.access_token;
+   config.headers["Authorization"] = "Bearer "  + access_token;
     NProgress.start();
     // Do something before request is sent
     return config;
