ac622a88bfdeec55774319fae94d9ca633602169
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-01 13:16:56 +0700

    82

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,6 +10,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Register from "./Auth/Register";
 import ListQuiz from "./User/ListQuiz";
+import DetailQuiz from "./User/DetailQuiz";
 const Layout = (props) => {
   return (
     <>
@@ -18,7 +19,7 @@ const Layout = (props) => {
           <Route index element={<HomePage />} />
           <Route path="/users" element={<ListQuiz />} />
         </Route>
-        <Route path="/quiz/:id" element={<ListQuiz />} />
+        <Route path="/quiz/:id" element={<DetailQuiz />} />
         <Route path="/admins" element={<Admin />}>
           <Route index element={<Dashboard />} />
           <Route path="manage-users" element={<ManagerUser />} />
