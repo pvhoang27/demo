6588e583cf0c3aa96ec9148ed1416205b7c28f81
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-01 13:27:45 +0700

    82

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 const DetailQuiz = (props) => {
     const params = useParams();
+    const quizId = params.id;
+
+    useEffect(() => {
+        //call api get detail quiz
+    }, [quizId]);
     console.log('>>> check params detail quiz: ', params);
   return <div className="detail-quiz-container">
     DetailQuiz
