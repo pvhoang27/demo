164c53a7fa83c0d1eda629aa256ef866815e3d5b
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-02 14:47:55 +0700

    86

--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -7,16 +7,15 @@ const Question = (props) => {
 
   return (
     <>
-      {data.image ?  (
+      {data.image ? (
         <div className="q-image">
           <img src={`data:image/jpeg;base64,${data.image}`} />
         </div>
-      )
-      : <div className="q-image">
-        </div>
-        }
+      ) : (
+        <div className="q-image"></div>
+      )}
       <div className="question">
-        question {index + 1} :{data.questionDescription} ?
+        question {index + 1} : {data.questionDescription} ?
       </div>
       <div className="answer">
         {data.answers &&
