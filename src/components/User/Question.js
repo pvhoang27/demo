0621d4e3a3e393ab3d311a613df57e1ec61deed9
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-02 14:12:18 +0700

    85

--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,23 +1,24 @@
 import _ from "lodash";
 const Question = (props) => {
-    const {data, index} = props;
-    if(_.isEmpty(data)){
-        return <></>;
-    }
+  const { data, index } = props;
+  if (_.isEmpty(data)) {
+    return <></>;
+  }
 
-  return(
-  <>
-  <div>
-    <img  src = {`data:image/jpeg;base64,${data.image}`}/>
-  </div>
-     <div className="question">question {index + 1} : 
-        {data.questionDescription} ? </div>
-          <div className="answer">
-            <div className="a-child">A.sfsfd</div>
-            <div className="a-child">B.sfsfd</div>
-            <div className="a-child">C.sfsfd</div>
-          </div>
-  </>
+  return (
+    <>
+      <div>
+        <img src={`data:image/jpeg;base64,${data.image}`} />
+      </div>
+      <div className="question">
+        question {index + 1} :{data.questionDescription} ?{" "}
+      </div>
+      <div className="answer">
+        <div className="a-child">A.sfsfd</div>
+        <div className="a-child">B.sfsfd</div>
+        <div className="a-child">C.sfsfd</div>
+      </div>
+    </>
   );
 };
 export default Question;
