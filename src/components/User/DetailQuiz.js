a83def6ca8887ab9624c54bb41507877d882d2a8
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-02 14:43:18 +0700

    86

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -82,6 +82,9 @@ const DetailQuiz = (props) => {
           <button className="btn btn-primary " onClick={() => handleNext()}>
             Next
           </button>
+          <button className="btn btn-warning " onClick={() => handleNext()}>
+            Finish
+          </button>
         </div>
       </div>
       <div className="right-content">count down</div>
