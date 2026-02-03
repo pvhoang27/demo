cb6f609bc2e90111e485b09004e827016a68a927
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-03 18:42:09 +0700

    91

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -44,82 +44,63 @@ const ManageQuiz = () => {
     <div className="quiz-container">
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Header>Manage Quizzes</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <div className="add-new">
+              <fieldset className="border rounded-3 p-3">
+                <legend className="float-none w-auto px-3">
+                  Add new quiz:
+                </legend>
+                <div className="form-floating mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="your quiz nme"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                  />
+                  <label>Name</label>
+                </div>
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="description..."
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                  />
+                  <label>Description</label>
+                </div>
+                <div className="my-3">
+                  <Select
+                    defaultValue={type}
+                    onChange={setType}
+                    options={options}
+                    placeholder={"Quiz type..."}
+                  />
+                </div>
+                <div className="more-actions" form-group>
+                  <label className="mb-1"> Upload image </label>
+                  <input
+                    type="file"
+                    className="form-control"
+                    onChange={(event) => handleChangeFile(event)}
+                  />
+                </div>
+                <div className="mt-3">
+                  <button
+                    onClick={() => handleSubmitQuiz()}
+                    className="btn btn-warning"
+                  >
+                    Save
+                  </button>
+                </div>
+              </fieldset>
+            </div>
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
 
-      <div className="title">Manage Quizzes</div>
-      <hr />
-      <div className="add-new">
-        <fieldset className="border rounded-3 p-3">
-          <legend className="float-none w-auto px-3">Add new quiz:</legend>
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="your quiz nme"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-            <label>Name</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="description..."
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-            />
-            <label>Description</label>
-          </div>
-          <div className="my-3">
-            <Select
-              defaultValue={type}
-              onChange={setType}
-              options={options}
-              placeholder={"Quiz type..."}
-            />
-          </div>
-          <div className="more-actions" form-group>
-            <label className="mb-1"> Upload image </label>
-            <input
-              type="file"
-              className="form-control"
-              onChange={(event) => handleChangeFile(event)}
-            />
-          </div>
-          <div className="mt-3">
-            <button
-              onClick={() => handleSubmitQuiz()}
-              className="btn btn-warning"
-            >
-              Save
-            </button>
-          </div>
-        </fieldset>
-      </div>
       <div className="list-detail">
         <TableQuiz />
       </div>
