e3c000965deb8753019357c75ccad9dbfe7cceee
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-03 08:35:40 +0700

    88

--- src/components/User/ModalResult.js
+++ src/components/User/ModalResult.js
@@ -7,7 +7,7 @@ const ModalResult = (props) => {
 
   const handleClose = () => setShow(false);
 
-  console.log(">>> check dataModalResult: ", dataModalResult);
+  console.log(">>> check dataModalResultmm: ", dataModalResult);
 
   return (
     <>
