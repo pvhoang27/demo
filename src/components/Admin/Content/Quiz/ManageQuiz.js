9ff9a0423b979e0b4bb2110d4585a01eeec03dfe
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-03 10:58:42 +0700

    89

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -1,46 +1,30 @@
-import './ManageQuiz.css';
+import "./ManageQuiz.scss";
 const ManageQuiz = () => {
   return (
     <div className="quiz-container">
       <div className="title">Manage Quizzes</div>
+      <hr />
       <div className="add-new">
-        <div class="form-floating mb-3">
-          <input
-            type="email"
-            class="form-control"
-            id="floatingInput"
-            placeholder="name@example.com"
-          />
-          <label for="floatingInput">Email address</label>
-        </div>
-        <div class="form-floating">
-          <input
-            type="password"
-            class="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-          />
-          <label for="floatingPassword">Password</label>
-        </div>
         <fieldset class="border rounded-3 p-3">
-          <legend class="float-none w-auto px-3">Personalia:</legend>
-          <label for="fname">First name:</label>
-          <input type="text" id="fname" name="fname" />
-          <br />
-          <br />
-          <label for="lname">Last name:</label>
-          <input type="text" id="lname" name="lname" />
-          <br />
-          <br />
-          <label for="email">Email:</label>
-          <input type="email" id="email" name="email" />
-          <br />
-          <br />
-          <label for="birthday">Birthday:</label>
-          <input type="date" id="birthday" name="birthday" />
-          <br />
-          <br />
-          <input type="submit" value="Submit" />
+          <legend class="float-none w-auto px-3">Add new quiz:</legend>
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
         </fieldset>
       </div>
       <div className="list-detail">table</div>
