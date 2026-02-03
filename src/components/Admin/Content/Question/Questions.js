ee537190062a18d5530fb425f989be26ff419282
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-03 20:47:33 +0700

    94

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -9,22 +9,30 @@ const Questions = (props) => {
     { value: "vanilla", label: "Vanilla" },
   ];
 
-  const[selectedQuiz, setSelectedQuiz] =  useState({});
+  const [selectedQuiz, setSelectedQuiz] = useState({});
   return (
     <div className="questions-container">
       <div className="title">Manage Questions</div>
       <div className="add-new-questions">
         <div className="col-6 from-group">
-        <label>Select Quiz: </label>
-
+          <label>Select Quiz: </label>
         </div>
         <Select
           defaultValue={selectedQuiz}
           onChange={setSelectedQuiz}
           options={options}
-          className="form-control"
-        
         />
+      </div>
+      <div>
+        
+        <div class="form-floating mb-3">
+          <input
+            type="type"
+            class="form-control"
+            placeholder="name@example.com"
+          />
+          <label>Description</label>
+        </div>
       </div>
     </div>
   );
