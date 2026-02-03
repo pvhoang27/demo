2075674b0d2335c828df29bdabefe7bfeb442dbe
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-03 11:03:18 +0700

    89

--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -23,9 +23,10 @@ import { DiReact } from "react-icons/di";
 import { MdDashboard } from "react-icons/md";
 
 import "./SideBar.scss";
-import { Link } from "react-router-dom";
+import { Link , useNavigate} from "react-router-dom";
 
 const SideBar = (props) => {
+    const navigate = useNavigate();
   const { image, collapsed, toggled, handleToggleSidebar } = props;
   return (
     <>
@@ -50,7 +51,9 @@ const SideBar = (props) => {
             }}
           >
             <DiReact size={"3em"} color={"00bfff"} />
-            <span>Hoi Dan IT</span>
+            <span onClick={() => navigate("/")}>
+              Hoang
+            </span>
           </div>
         </SidebarHeader>
 
