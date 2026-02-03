18b3ccf6589e05a0ea75f44b0a7ef5b95a7ce644
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-03 20:16:11 +0700

    94

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
+import Select from "react-select";
+
 const Questions = (props) => {
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
+  const[selectedQuiz, setSelectedQuiz] =  useState({});
   return (
-  <div className="questions-container">
-    <div className="title">
-        Manage Questions
+    <div className="questions-container">
+      <div className="title">Manage Questions</div>
+      <div className="add-new-questions">
+        <Select
+          defaultValue={selectedQuiz}
+          onChange={setSelectedQuiz}
+          options={options}
+        />
+      </div>
     </div>
-    <div className="add-new-questions">
-        
-    </div>
-    </div>
-    );
-}
+  );
+};
 export default Questions;
