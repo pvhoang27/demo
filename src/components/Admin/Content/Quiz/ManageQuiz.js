c562a6b796518da6a4f1d4090a6449f2335dad48
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-03 10:47:39 +0700

    89

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -1,6 +1,16 @@
 const ManageQuiz = () => {
   return (
-  <div className="quiz-container">Manage Quiz Component</div>
+  <div className="quiz-container">
+    <div className="title">
+        Manage Quizzes
+    </div>
+    <div className="add-new">
+
+    </div>
+    <div className="list-detail">
+        table
+    </div>
+    </div>
 );
 }
 
