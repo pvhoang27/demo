0b978357eddaf6cdabace4b1dd200e11e2cb88e8
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-05 18:32:49 +0700

    98

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -51,6 +51,10 @@ const Questions = (props) => {
   const fetchQuiz = async () => {
     let res = await getAllQuizForAdmin();
     if (res && res.EC === 0) {
+      let newQuiz = res.DT.map((item) => {
+        return { 
+          value: item.id, label: item.name };
+      });
       setListQuiz(res.DT);
     }
   };
