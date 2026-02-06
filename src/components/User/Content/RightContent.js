acd139a6b6e20e335f73c9b0444ad54cdb37b305
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-06 14:53:56 +0700

    103

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,11 +1,14 @@
 const RightContent = (props) => {
-    return(
-        <>
-            <div className="main-timer">
-                10:10
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="main-timer">10:10</div>
+      <div className="main-question">
+        <div className="question">1</div>
+        <div className="question">2</div>
+        <div className="question">3</div>
+      </div>
+    </>
+  );
+};
 
 export default RightContent;
