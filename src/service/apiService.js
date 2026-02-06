180a6b1ba06d5675ae69fa8c8bd00183e3628404
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-06 14:12:26 +0700

    102

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -121,6 +121,10 @@ const postAssignQuiz = (quizId, userId) => {
 const getQuizWithQA = (quizId) => {
   return axios.get(`api/v1/quiz-with-qa/${quizId}`);
 };
+
+const postUpsertQA = (data) => {
+  return axios.post(`api/v1/quiz-upsert-qa`, { ...data });
+}
 export {
   postCreateNewUser,
   getAllUsers,
@@ -140,4 +144,5 @@ export {
   postCreateNewAnswerForQuestion,
   postAssignQuiz,
   getQuizWithQA,
+  postUpsertQA,
 };
