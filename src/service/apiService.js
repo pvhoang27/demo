60dc4c3bbf1f49ac3e2a36d594ac9472cca3b3ca
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-06 09:31:04 +0700

    101

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,47 +1,45 @@
-import axios from '../utils/axiosCustomize';
+import axios from "../utils/axiosCustomize";
 
 const postCreateNewUser = (email, password, username, role, image) => {
-    //submit data
-    const data = new FormData();
-    data.append('email', email);
-    data.append('password', password);
-    data.append('username', username);
-    data.append('role', role);
-    data.append('userImage', image);
-    return axios.post('api/v1/participant', data);
-}
+  //submit data
+  const data = new FormData();
+  data.append("email", email);
+  data.append("password", password);
+  data.append("username", username);
+  data.append("role", role);
+  data.append("userImage", image);
+  return axios.post("api/v1/participant", data);
+};
 
 const getAllUsers = () => {
-    return axios.get('api/v1/participant/all');
-}
+  return axios.get("api/v1/participant/all");
+};
 
 const putUpdateUser = (id, username, role, image) => {
-    //submit data
-    const data = new FormData();
-    data.append('id', id);
-    data.append('username', username);
-    data.append('role', role);
-    data.append('userImage', image);
-    return axios.put('api/v1/participant', data);
-}
+  //submit data
+  const data = new FormData();
+  data.append("id", id);
+  data.append("username", username);
+  data.append("role", role);
+  data.append("userImage", image);
+  return axios.put("api/v1/participant", data);
+};
 
 const deleteUser = (userId) => {
-    return axios.delete('api/v1/participant', { data: { id: userId } });
-}
+  return axios.delete("api/v1/participant", { data: { id: userId } });
+};
 
 const getUserWithPaginate = (page, limit) => {
-    return axios.get(`api/v1/participant?page=${page}&limit=${limit}`);
-}
+  return axios.get(`api/v1/participant?page=${page}&limit=${limit}`);
+};
 
 const postLogin = (userEmail, userPassword) => {
-    return axios.post(`/api/v1/login`,
-        {
-            email: userEmail,
-            password: userPassword,
-            delay: 5000 //ms
-        }
-    );
-}
+  return axios.post(`/api/v1/login`, {
+    email: userEmail,
+    password: userPassword,
+    delay: 5000, //ms
+  });
+};
 
 // const postLogin1 = (email, password) => {
 //     return axios.post(`/api/v1/login`,
@@ -51,74 +49,88 @@ const postLogin = (userEmail, userPassword) => {
 // }
 
 const postRegister = (email, password, username) => {
-    return axios.post(`/api/v1/register`,
-        { email, password, username }
-    );
-}
+  return axios.post(`/api/v1/register`, { email, password, username });
+};
 
 const getQuizByUser = () => {
-    return axios.get('/api/v1/quiz-by-participant');
-}
+  return axios.get("/api/v1/quiz-by-participant");
+};
 
 const getDataQuiz = (id) => {
-    return axios.get(`/api/v1/questions-by-quiz?quizId=${id}`);
-}
+  return axios.get(`/api/v1/questions-by-quiz?quizId=${id}`);
+};
 
 const postSubmitQuiz = (data) => {
-    return axios.post(`/api/v1/quiz-submit`, { ...data });
-}
-
+  return axios.post(`/api/v1/quiz-submit`, { ...data });
+};
 
 const postCreateNewQuiz = (description, name, difficulty, image) => {
-    const data = new FormData();
-    data.append('description', description);
-    data.append('name', name);
-    data.append('difficulty', difficulty);
-    data.append('quizImage', image);
-    return axios.post('api/v1/quiz', data);
-}
+  const data = new FormData();
+  data.append("description", description);
+  data.append("name", name);
+  data.append("difficulty", difficulty);
+  data.append("quizImage", image);
+  return axios.post("api/v1/quiz", data);
+};
 
 const getAllQuizForAdmin = () => {
-    return axios.get(`/api/v1/quiz/all`);
-}
-
+  return axios.get(`/api/v1/quiz/all`);
+};
 
 const putUpdateQuizForAdmin = (id, name, description, difficulty, image) => {
-    const data = new FormData();
-    data.append('id', id);
-    data.append('description', description);
-    data.append('name', name);
-    data.append('difficulty', difficulty);
-    data.append('quizImage', image);
-    return axios.put('api/v1/quiz', data);
-}
+  const data = new FormData();
+  data.append("id", id);
+  data.append("description", description);
+  data.append("name", name);
+  data.append("difficulty", difficulty);
+  data.append("quizImage", image);
+  return axios.put("api/v1/quiz", data);
+};
 
 const deleteQuizForAdmin = (id) => {
-    return axios.delete(`/api/v1/quiz/${id}`);
-}
+  return axios.delete(`/api/v1/quiz/${id}`);
+};
 
 const postCreateNewQuestionForQuiz = (quiz_id, description, image) => {
-    const data = new FormData();
-    data.append('quiz_id', quiz_id);
-    data.append('description', description);
-    data.append('questionImage', image);
-    return axios.post('api/v1/question', data);
-}
+  const data = new FormData();
+  data.append("quiz_id", quiz_id);
+  data.append("description", description);
+  data.append("questionImage", image);
+  return axios.post("api/v1/question", data);
+};
 
-const postCreateNewAnswerForQuestion = (description, correct_answer, 
-    question_id) => {
+const postCreateNewAnswerForQuestion = (
+  description,
+  correct_answer,
+  question_id,
+) => {
+  return axios.post("api/v1/answer", {
+    description,
+    correct_answer,
+    question_id,
+  });
+};
 
-
-    return axios.post('api/v1/answer', {
-
-    });
-}
+const postAssignQuiz = (quizId, userId) => {
+    return axios.post("api/v1/answer", {
+    quizId, userId
+  });
+};
 export {
-    postCreateNewUser, getAllUsers, putUpdateUser,
-    deleteUser, getUserWithPaginate, postLogin,
-    postRegister, getQuizByUser, getDataQuiz,
-    postSubmitQuiz, postCreateNewQuiz,
-    getAllQuizForAdmin, putUpdateQuizForAdmin, deleteQuizForAdmin,
-    postCreateNewQuestionForQuiz,
-    postCreateNewAnswerForQuestion
-}
+  postCreateNewUser,
+  getAllUsers,
+  putUpdateUser,
+  deleteUser,
+  getUserWithPaginate,
+  postLogin,
+  postRegister,
+  getQuizByUser,
+  getDataQuiz,
+  postSubmitQuiz,
+  postCreateNewQuiz,
+  getAllQuizForAdmin,
+  putUpdateQuizForAdmin,
+  deleteQuizForAdmin,
+  postCreateNewQuestionForQuiz,
+  postCreateNewAnswerForQuestion,
+};
