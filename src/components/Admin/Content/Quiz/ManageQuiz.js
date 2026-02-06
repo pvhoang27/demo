15e4110588c83205585a546119d548da66709e9e
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-06 08:55:05 +0700

    100

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import { set } from "nprogress";
 import TableQuiz from "./TableQuiz";
 import Accordion from "react-bootstrap/Accordion";
+import QuizQA from "./QuizQA";
 // ...rest of code...
 
 const options = [
@@ -106,7 +107,7 @@ const ManageQuiz = (props) => {
         <Accordion.Item eventKey="1">
           <Accordion.Header>Update Q/A Quizzes</Accordion.Header>
           <Accordion.Body>
-           11111
+           <QuizQA />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
