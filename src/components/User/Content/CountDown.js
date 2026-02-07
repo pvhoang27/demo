98bb91bc343eda9d99457a2d7dbbe8f92f5a69ad
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 09:17:28 +0700

    104

--- src/components/User/Content/CountDown.js
+++ src/components/User/Content/CountDown.js
@@ -2,6 +2,13 @@ import { useState, useEffect } from "react";
 
 const CountDown = (props) =>{
     const[count,setCount] = useState(10);
+
+    useEffect( () =>{
+        setCount(count -1)
+        setInterval( () => {
+            console.log('run bts')
+        }, 1000)
+    }, [])
     return(
         <div className="countdown-container">
             {count}
