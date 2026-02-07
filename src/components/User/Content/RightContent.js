41fe66efa01f0c1eb315d012684207269ab147d6
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 10:22:28 +0700

    105

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -25,6 +25,11 @@ const RightContent = (props) => {
   const handleClickQuestion = (question, index) => {
     if(refDiv.current){
       console.log(">>> check refDiv: ", refDiv.current);
+      refDiv.current.forEach(item => {
+        if(item && item.className === "question clicked"){
+          item.className = "question";
+        }
+      })
     }
  
     refDiv.current[index].className = "question clicked";
