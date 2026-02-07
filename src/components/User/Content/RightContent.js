875006d41cff9bc28a01c6c781b62b555d867589
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 10:00:30 +0700

    105

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -11,7 +11,7 @@ const RightContent = (props) => {
     console.log(index, question);
     //check answered
     if (question && question.answers.length > 0) {
-      let isUnAnswered = question.answers.some(
+      let isUnAnswered = question.answers.find(
         (a) => a.isSelected === false);
       console.log(">>> check isUnAnswered: ", isUnAnswered, index);
       if (isUnAnswered === false) {
