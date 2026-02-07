9782f9b5c9948a627dfedfae0b4dfb9e7ed78179
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 09:31:51 +0700

    104

--- src/components/User/Content/CountDown.js
+++ src/components/User/Content/CountDown.js
@@ -3,10 +3,21 @@ import { useState, useEffect } from "react";
 const CountDown = (props) => {
   const [count, setCount] = useState(10);
 
-  const a = new Date(SECONDS * 1000).toISOString().substring(11, 16)
+  const toHHMMSS = (secs) => {
+    const sec_num = parseInt(secs, 10);
+    const hours = Math.floor(sec_num / 3600);
+    const minutes = Math.floor(sec_num / 60) % 60;
+    const seconds = sec_num % 60;
+
+    return [hours, minutes, seconds]
+      .map((v) => (v < 10 ? "0" + v : v))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
+  };
+
+  //   console.log(toHHMMSS(500));
 
   useEffect(() => {
-
     if (count === 0) return;
     setCount(count - 1);
     const timer = setInterval(() => {
@@ -18,11 +29,12 @@ const CountDown = (props) => {
     // }, 5000);
 
     return () => {
-        clearInterval(timer);
-    }
-
+      clearInterval(timer);
+    };
   }, [count]);
-  return <div className="countdown-container">{count}</div>;
+  return <div className="countdown-container">
+    {toHHMMSS(count)}
+    </div>;
 };
 
 export default CountDown;
