969e3c439f7e6dce8f795b7ed24c3e9783efad46
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 09:44:56 +0700

    104

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -160,6 +160,7 @@ const DetailQuiz = (props) => {
       <div className="right-content">
         <RightContent 
         dataQuiz={dataQuiz}
+        handleFinishQuiz={handleFinishQuiz}
         />
         </div>
       <ModalResult 
