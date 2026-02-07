23e3b23031df98ae9c05e2fe3870abe1897352d8
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 18:10:13 +0700

    107

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -124,11 +124,14 @@ const getQuizWithQA = (quizId) => {
 
 const postUpsertQA = (data) => {
   return axios.post(`api/v1/quiz-upsert-qa`, { ...data });
-}
+};
 
-const logout = (email ,refresh_token ) => {
-
-}
+const logout = (email, refresh_token) => {
+  return axios.put("api/v1/logout", {
+    email,
+    refresh_token,
+  });
+};
 export {
   postCreateNewUser,
   getAllUsers,
