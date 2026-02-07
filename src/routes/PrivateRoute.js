dc37fca4421dcf34426886962924980ab7cefeb9
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 14:50:54 +0700

    106

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,11 +2,10 @@ import Test1 from "./Test1";
 import Test2 from "./Test2";
 
 const PrivateRoute = (props) => {
+    console.log('>>> check props in private route: ', props);
   return (
-  <div>Private Route
-    <Test2 >
-        <Test1 />
-    </Test2>
+  <div>
+
   
   </div>
   );
