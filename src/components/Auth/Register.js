0eb9a9393bf77bd5e1130f8da9c4c5ab81ddf202
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 18:56:34 +0700

    108

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import { postRegister } from "../../service/apiService";
+import Language from "../Header/Language";
 const Register = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -49,6 +50,7 @@ const Register = (props) => {
       <div className="header">
         <span> Already have an account?</span>
         <button onClick={() => navigate("/login")}>Log in</button>
+        <Language />
       </div>
       <div className="title col-4 mx-auto">Hỏi Dân IT &amp; Eric</div>
       <div className="welcome col-4 mx-auto">Start your journey?</div>
