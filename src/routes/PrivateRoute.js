bc6e04b01efdb8a79fbc212adda168450c307764
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 14:56:37 +0700

    106

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -5,9 +5,10 @@ const PrivateRoute = (props) => {
   console.log(">>> check props in private route: ", props);
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const navigate = useNavigate();
-
+    console.log(">>> check isAuthenticated: ", isAuthenticated);
   if (!isAuthenticated) {
     navigate("/login");
+    return <></>;
   }
   return <>{props.children}</>;
 };
