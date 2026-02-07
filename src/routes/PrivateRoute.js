b43828152298253ff55b626c04d230479f0c27d7
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 14:59:51 +0700

    106

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -2,10 +2,8 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = (props) => {
-  console.log(">>> check props in private route: ", props);
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
 //   const navigate = useNavigate();
-    console.log(">>> check isAuthenticated: ", isAuthenticated);
   if (!isAuthenticated) {
     return <Navigate to="/login"></Navigate>;
   }
