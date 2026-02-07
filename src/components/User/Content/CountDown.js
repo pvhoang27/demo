a1c2560e4577dfd0b9995dbf8e2b46220dc98f7f
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 09:44:01 +0700

    104

--- src/components/User/Content/CountDown.js
+++ src/components/User/Content/CountDown.js
@@ -3,7 +3,10 @@ import { useState, useEffect } from "react";
 const CountDown = (props) => {
   const [count, setCount] = useState(5);
   useEffect(() => {
-    if (count === 0) return;
+    if (count === 0) {
+      props.onTimeUp();
+      return;
+    }
 
     const timer = setInterval(() => {
       setCount(count - 1);
--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -2,17 +2,22 @@ import CountDown from "./CountDown";
 
 const RightContent = (props) => {
   const { dataQuiz } = props;
+  const onTimeUp = () => {
+    alert("Time is up!");
+  }
   console.log(">>> check dataQuiz right content: ", dataQuiz);
   return (
     <>
       <div className="main-timer">
-        <CountDown />
+        <CountDown onTimeUp={onTimeUp} />
       </div>
       <div className="main-question">
         {dataQuiz &&
           dataQuiz.length > 0 &&
           dataQuiz.map((item, index) => {
-            return <div key ={`question-abc-${index}`} className="question">{index + 1}</div>;
+            return <div 
+            key ={`question-abc-${index}`} 
+            className="question">{index + 1}</div>;
           })}
       </div>
     </>
