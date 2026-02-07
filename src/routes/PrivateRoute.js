9c4521d293a9f50be3c855e965c6f391d619da2b
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 14:47:40 +0700

    106

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,6 +1,14 @@
+import Test1 from "./Test1";
+import Test2 from "./Test2";
+
 const PrivateRoute = (props) => {
   return (
-  <div>Private Route</div>
+  <div>Private Route
+    <Test2 >
+        <Test1 />
+    </Test2>
+  
+  </div>
   );
 }
 
