8387e1179b7e7a8e708a2997c36d3942ecdbd7be
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-07 09:46:04 +0700

    104

--- src/components/User/Content/CountDown.js
+++ src/components/User/Content/CountDown.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const CountDown = (props) => {
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(300);
   useEffect(() => {
     if (count === 0) {
       props.onTimeUp();
