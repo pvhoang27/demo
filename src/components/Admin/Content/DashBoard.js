c97b09a1890986580d281e0f918df01d7f2ef0d8
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-08 10:13:21 +0700

    111

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -55,8 +55,9 @@ const Dashboard = (props) => {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
+              <Bar dataKey="Qz" fill="#8884d8" />
+              <Bar dataKey="Qs" fill="#82ca9d" />
+              <Bar dataKey="As" fill="#82ca9d" />
             </BarChart>
           </ResponsiveContainer>
         </div>
