b90695d18a6e8e6497f64c7d20b3d7d3a7f764d0
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-08 09:50:54 +0700

    110

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -5,10 +5,10 @@ const Dashboard = (props) => {
       <div className="title">Dashboard</div>
       <div className="content">
         <div className="c-left">
-            <div>Total users</div>
-            <div>Total quizess</div>
-            <div>Total questions</div>
-            <div>Total answers</div>
+            <div className="child">Total users</div>
+            <div className="child">Total quizess</div>
+            <div className="child">Total questions</div>
+            <div className="child">Total answers</div>
         </div>
         <div className="c-right">
 
